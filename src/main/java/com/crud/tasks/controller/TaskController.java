0e70a002ef8c2a624cd0cef3cfc75bed4dac433c
klaudilag <klaudilag@wp.pl>
2023-06-13 17:58:12 +0200

    Dodanie controllera dla zadania 14.4

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,33 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+@RequestMapping("/v1/tasks")
+public class TaskController {
+    @GetMapping
+    public List<TaskDto> getTasks(){
+        return new ArrayList<>();
+    }
+    @GetMapping
+    public TaskDto getTask(Long taskid){
+        return new TaskDto(1L, "test title", "test_content")
+    }
+    @DeleteMapping
+    public void deleteTask(Long taskid){
+    }
+    @PutMapping
+    public TaskDto updateTask(TaskDto taskDto){
+        return new TaskDto(1L, "edited title", "edited content")
+    }
+    @PostMapping
+    public void createTask(TaskDto taskDto){
+
+    }
+
+}
